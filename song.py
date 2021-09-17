e376e9d8f85b3804cc9865160cf2df84f8d25a42
ojukeihpos@gmail.com <ojukeihpos@gmail.com>
2021-09-17 19:14:26 -0400

    Added docstring

--- song.py
+++ song.py
@@ -3,6 +3,20 @@ import YTDLSource
 class Song:
     __slots__ = ('source')
 
+    """
+    Holds info for a valid query performed by YTDLSource.py.
+    Asserted that the query was valid, otherwise a Song object would never
+    be instantiated.
+
+    Attributes:
+        title: Name of the song
+        url: Link to the song of format 'https://www.youtube.com/watch?v=%s' % (VId) where VId is the ID of the song on YouTube
+        thumbnail: Thumbnail.
+        requester: The discord.context.message.author who queried for the song.
+        uploader: Person who uploaded the video onto YouTube
+        duration: Length of the song represented by format {}h{}m{}s
+    """
+
     def __init__(self, source: YTDLSource):
         self.source = source
 
